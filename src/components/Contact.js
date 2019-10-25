9433a55c0ded12a60922a8b55d9021c502857c7f
Alexandra Ferdinand <alexxkf@gmail.com>
2019-10-25 09:48:07 -0400

    adding routing to ind job detail

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,25 @@
-import { Link } from 'react-router-dom'
-import React from 'react'
-import _ from 'lodash'
+import { Link } from "react-router-dom";
+import React from "react";
+import _ from "lodash";
 
 // render the contact details page
-const Contact = ({ props, contacts }) => {
-  const contact = _.find(contacts, { id: parseInt(props.match.params.id, 10) });
+const Job = ({ props, jobs }) => {
+  const clickedJob = _.find(jobs, { id: parseInt(props.match.params.id, 10) });
 
-  if (!contact) {
-    return <div id="no-contact-msg">Sorry, but the contact was not found</div>
+  if (!job) {
+    return <div id="no-contact-msg">Sorry, but the job was not found</div>;
   }
   return (
-    <div id="contact">
-      <h3>Name: {contact.name}</h3>
-      <h3>Phone: {contact.phone}</h3>
-      <h3>email: {contact.email}</h3>
+    <div id="job">
+      <h3>Job Title: {job.MatchedObjectDescriptor.PositionTitle}</h3>
+      <h3>
+        Job Summary: {job.MatchedObjectDescriptor.UserArea.Details.JobSummary}
+      </h3>
       <div>
-        <Link to='/contacts'>Back</Link>
+        <Link to="/search">Back</Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Job;
